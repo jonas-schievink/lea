4967e596e9176621b69b657c9fefb4bee95d9fda
jonas-schievink <jonas@schievink.net>
2015-02-05 22:18:14 +0100

    Added simple expression benchmark

--- benches/parser.rs
+++ benches/parser.rs
@@ -5,14 +5,21 @@ extern crate lea;
 
 use test::Bencher;
 
-use lea::parser::block;
+use lea::parser::*;
 
 #[bench]
-fn parse_simple(b: &mut Bencher) {
+fn bench_simple(b: &mut Bencher) {
     b.iter(|| {
         block(r#"
 local i, j, k = 0, 1, 2+3
 i, j = j, i*2
 "#).unwrap();
+    });
+}
+
+#[bench]
+fn bench_expression(b: &mut Bencher) {
+    b.iter(|| {
+        expression("f(1,2,3,4,5,6,6,7,87,8,9,9,5,5,4)").unwrap();
     });
 }
