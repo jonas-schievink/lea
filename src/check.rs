d0b91e2275d97bc1fcd93803b2b342f837085720
Jonas Schievink <jonas@schievink.net>
2015-02-07 18:11:02 +0100

    Some work on check.rs

--- src/check.rs
+++ src/check.rs
@@ -15,7 +15,45 @@ fn mkerr(msg: &'static str, detail: String) -> CheckError {
     }
 }
 
+struct Checker {
+    res: Result<(), CheckError>,
+    forlvl: u32,
+}
+
+impl Visitor for Checker {
+    fn visit_stmt(&mut self, s: &mut Stmt) {
+        if let Ok(..) {
+            self.res = match *s {
+                SBreak => {
+                    if self.forlvl == 0 {
+                        Err(mkerr("Use of `break` outside of for loop"))
+                    } else { Ok(()) }
+                },
+                _ => { Ok(()) },
+            };
+
+            walk_stmt(s, self);
+        }
+    }
+}
+
 /// Checks that the passed block contains only valid statements.
-pub fn check_block(block: &Block) -> Result<(), CheckError> {
-    Err(mkerr("test", "detail".to_string()))
+pub fn check_block(block: &mut Block) -> Result<(), CheckError> {
+    let ch = Checker {
+        res: Ok(()),
+        forlvl: 0,
+    };
+    walk_block(block, &mut ch);
+    ch.res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use parse::block;
+
+    #[test]
+    fn test() {
+        assert!(check_block(block("break")).is_err());
+    }
 }
