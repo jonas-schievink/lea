510079749f4b1e2b1c259714811d554916a0b830
Travis CI <jonas@schievink.net>
2015-06-20 00:36:19 +0200

    check: remove detail field
    and don't merge errors on recursion (use the current instance instead of
    creating a new one for each function)

--- src/lea-compiler/check.rs
+++ src/lea-compiler/check.rs
@@ -6,14 +6,12 @@ use ast::visit::*;
 
 use term::Terminal;
 
-use std::fmt;
 use std::io::{self, Write};
 
 
 #[derive(Clone, Debug)]
 pub struct CheckError {
     pub msg: &'static str,
-    pub detail: Option<String>,
     pub span: Span,
 }
 
@@ -21,28 +19,12 @@ impl CheckError {
     /// Prints this `CheckError` to a terminal.
     pub fn format<W: Write>(&self, code: &str, source_name: &str, t: &mut Terminal<Output=W>)
     -> io::Result<()> {
-        let mut msg = self.msg.to_string();
-        if let Some(ref detail) = self.detail {
-            msg.push_str(detail.as_ref());
-        }
-        try!(self.span.print_with_err(code, source_name, msg.as_ref(), t));
+        try!(self.span.print_with_err(code, source_name, self.msg, t));
 
         Ok(())
     }
 }
 
-impl fmt::Display for CheckError {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        match self.detail {
-            None => {
-                fmt.write_fmt(format_args!("{}", self.msg))
-            },
-            Some(ref detail) => {
-                fmt.write_fmt(format_args!("{}: {}", self.msg, detail))
-            }
-        }
-    }
-}
 
 struct Checker {
     errs: Vec<CheckError>,
@@ -57,7 +39,6 @@ impl <'a> Visitor<'a> for Checker {
                 if self.looplvl == 0 {
                     self.errs.push(CheckError {
                         msg: "use of `break` outside of loop",
-                        detail: None,
                         span: s.span,
                     });
                 }
@@ -77,7 +58,6 @@ impl <'a> Visitor<'a> for Checker {
                 if !self.vararg_func {
                     self.errs.push(CheckError {
                         msg: "use of ... outside varargs function",
-                        detail: None,
                         span: e.span,
                     });
                 }
@@ -87,10 +67,15 @@ impl <'a> Visitor<'a> for Checker {
     }
 
     fn visit_func(&mut self, func: &Function) {
-        let res = check_func(func);
-        if let Err(mut new_errs) = res {
-            self.errs.append(&mut new_errs);
-        }
+        let varargs_old = self.vararg_func;
+        let looplvl_old = self.looplvl;
+        self.vararg_func = func.varargs;
+        self.looplvl = 0;
+
+        self.visit_block(&func.body);
+
+        self.vararg_func = varargs_old;
+        self.looplvl = looplvl_old;
     }
 }
 
@@ -123,6 +108,11 @@ mod tests {
         assert!(check_func(&parse_main("break").unwrap()).is_err());
         assert!(check_func(&parse_main("for i=0,1 do break end").unwrap()).is_ok());
         assert!(check_func(&parse_main("i = ...").unwrap()).is_ok());
+        assert!(check_func(&parse_main(r#"
+for i=0,1 do
+    function i() break end
+end
+"#).unwrap()).is_err());
         assert!(check_func(&parse_main(r#"
 function f(a)
     a = ...
