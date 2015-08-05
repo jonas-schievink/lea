11c26bb1041541a0e8234e9df42b65adfe7cdb79
Jonas Schievink <jonas@schievink.net>
2015-08-06 00:43:18 +0200

    Fix travis

--- travis/test.sh
+++ travis/test.sh
@@ -4,7 +4,7 @@ set -e
 
 # Run all unit and integration tests for all crates inside the "src" directory.
 
-for proj in $(ls src); do
+for proj in $(ls src | grep lea); do
     echo "[TESTING] $proj"
     cargo build -p "$proj"
     cargo test -p "$proj"
